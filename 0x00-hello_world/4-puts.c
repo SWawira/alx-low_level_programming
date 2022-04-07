b9f7e4f96675f590584255b1416a7c3c9ad0b92a
SWawira <sharonwawira72@gmail.com>
2022-04-07 11:54:53 -0700

    Puts function to output a string

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
  *
- * main is the entry point
+ * main: the entry point
  *
  * return 0 means success
  *
  */
 int main(void)
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
