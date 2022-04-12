649f51ff5355c6ce1dcd213c901a2130835217a2
SWawira <sharonwawira72@gmail.com>
2022-04-12 09:34:06 -0700

    Adding file 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Prints _putchar followed by a new line.
+ *
+ * Return : Always 0 (Success)
+ */
+
+int main(void)
+{
+	char pchar[8] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		putchar(pchar[i]);
+	}
+	putchar('\n');
+	return (0);
+}
